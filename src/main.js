792ff997ac6f386e34a9c7c0f0c90847aa9be242
Sarah Fauziah <93574833+sarahfh12@users.noreply.github.com>
2022-02-22 10:47:48 +0700

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-Vue.config.productionTip = false
+import VueSupabase from "vue-supabase";
+
+Vue.use(VueSupabase, {
+  supabaseUrl: "https://wyqkwblhpqjlbmarfzes.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyOTk4NTM1LCJleHAiOjE5NTg1NzQ1MzV9.LkeDwA0BiB-4Caaqaqfsm62YbEqzMuvWS21u91NFtRw",
+  supabaseOptions: {},
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
