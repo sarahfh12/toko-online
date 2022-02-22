9d2280dca4b57ee9133d2b2b491b1d9a9155f5d4
Sarah Fauziah <93574833+sarahfh12@users.noreply.github.com>
2022-02-22 10:22:23 +0700

    Add files via upload

---
+++ src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Contact</h1>
+      <peta />
+    </div>
+  </div>
+</template>
+
+<script>
+import peta from '../components/Peta.vue'
+
+export default{
+    components: {
+      peta,
+    }
+  }
+</script>
+
